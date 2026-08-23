82d2236668fb703b4c1723eaccbf3543b54e04dc
M Juma <qasimimohammad819@gmail.com>
2026-08-23 18:11:23 +0200

    update navbar component and add NavItem Type

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,10 @@
 import { Link } from "react-router-dom";
+import type { NavItem } from "../types/navbar";
+
+const navItem: NavItem[] = [
+  { name: "HOME", path: "/" },
+  { name: "ABOUT", path: "/about" },
+];
 const Navbar = () => {
   return (
     <div className="bg-sky-950 text-white font-medium">
@@ -6,15 +12,11 @@ const Navbar = () => {
       <nav className="max-w-7xl mx-auto flex justify-between items-center">
         <h1>LOGO</h1>
         <ul className="flex justify-between items-center gap-6 p-6">
-          <li>
-            <Link to="/">HOME</Link>
-          </li>
-          <li>
-            <Link to="/about">ABOUT</Link>
-          </li>
-          <li>
-            <Link to="/contact">CONTACT</Link>
-          </li>
+          {navItem.map((item) => (
+            <li>
+              <Link to={item.path}>{item.name}</Link>
+            </li>
+          ))}
         </ul>
         <h1>Login</h1>
       </nav>
