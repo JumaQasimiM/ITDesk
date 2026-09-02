6a435f6c7d9666e32f7668a40bcd63a74693ae41
M Juma <qasimimohammad819@gmail.com>
2026-09-02 09:23:04 +0200

    update desgin recent tikit

--- src/features/dashboard/components/RecentTikits.tsx
+++ src/features/dashboard/components/RecentTikits.tsx
@@ -2,10 +2,12 @@ import RecentTikitCard from "./RecentTikitCard";
 
 const RecentTickets = () => {
   return (
-    <div className="grid grid-cols-4 gap-5">
-      {[1, 2, 3, 4].map((card) => (
-        <RecentTikitCard />
-      ))}
+    <div className="mx-w-7xl mx-auto">
+      <div className="grid grid-cols-4 gap-5">
+        {[1, 2, 3, 4].map((card) => (
+          <RecentTikitCard />
+        ))}
+      </div>
     </div>
   );
 };
