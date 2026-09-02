19d87236db85b524f87d3ed3c8784eaf38921ca2
M Juma <qasimimohammad819@gmail.com>
2026-09-02 09:19:38 +0200

    create recentTikitCArd.tsx

---
+++ src/features/dashboard/components/RecentTikitCard.tsx
@@ -0,0 +1,11 @@
+// recent
+
+const RecentTikitCard = () => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-3 bg-teal-500 p-6 ">
+      <h1>CARD</h1>
+    </div>
+  );
+};
+
+export default RecentTikitCard;
